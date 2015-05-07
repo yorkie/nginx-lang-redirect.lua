f72a80c425a40e1ac3e6132ff5154d3c201c38f3
Yazhong Liu <yorkiefixer@gmail.com>
2015-05-07 23:00:41 +0800

    fix bugs

--- lang.lua
+++ lang.lua
@@ -26,44 +26,47 @@
 --                                              --
 --------------------------------------------------
 
-local default_lang = ngx.var.default_lang or "cn"
+local default_lang = ngx.var.default_lang or "zh"
 local lang_header = ngx.var.http_accept_language
-local parsed_uri, _, _ = ngx.re.sub(ngx.var.uri, "/$", "")
+local parsed_uri, _, _ = ngx.re.sub(ngx.var.uri, "(en|zh)", "")
 local lang_corrects = { us = "en",
                         en = "en",
-                        cn = "cn",
-                        zh = "cn",
-                        fr = "fr" }
+                        cn = "zh",
+                        zh = "zh" }
 
-local has_lang_at_uri, _ = ngx.re.match(ngx.var.uri, "^/(en|cn|fr)", "i")
-if ( has_lang_at_uri ) then
-    ngx.req.set_uri(parsed_uri)
-    return
+local m_uri, _ = ngx.re.match(ngx.var.uri, "(en|zh)(.*)", "")
+if m_uri then
+  if m_uri[1] == default_lang then
+    ngx.req.set_uri("/" .. m_uri[2])
+  else
+    ngx.req.set_uri("/en/" .. m_uri[2])
+  end
+  -- direct return if check /(en|zh)/..
+  return
 end
 
-if ( lang_header == nil ) then
-    ngx.redirect( default_lang:lower() .. parsed_uri )
-    return
+if lang_header == nil then
+  return
 end
 
 local cleaned = ngx.re.sub(lang_header, "^.*:", "")
 local options = {}
 local iterator, err = ngx.re.gmatch(cleaned, "\\s*([a-z]+(?:-[a-z])*)\\s*(?:;q=([0-9]+(.[0-9]*)?))?\\s*(,|$)", "i")
 for m, err in iterator do
-    local lang = lang_corrects[m[1]:lower()]
-    local priority = 1
-    if m[2] ~= nil then
-        priority = tonumber(m[2])
-        if priority == nil then
-            priority = 1
-        end
-    end
-    table.insert(options, {lang, priority})
+  local lang = lang_corrects[m[1]:lower()]
+  local priority = 1
+  if m[2] ~= nil then
+    priority = tonumber(m[2])
+    if priority == nil then priority = 1 end
+  end
+  table.insert(options, {lang, priority})
 end
 
 table.sort(options, function(a,b) return b[2] < a[2] end)
 
 for index, lang in pairs(options) do
-    ngx.redirect( lang[1] .. parsed_uri )
-    return
+  if lang[1]:lower() ~= default_lang then
+    ngx.req.set_uri("/en/" .. parsed_uri)
+    break
+  end
 end
